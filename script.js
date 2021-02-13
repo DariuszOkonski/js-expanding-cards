5f6f85c519e6215510c31b9bacac5c9fba6f70e0
Dariusz Okonski <darek200180@gmail.com>
2021-02-13 16:39:06 +0000

    add script

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello world')
+const boxes = document.querySelectorAll('.box')
+
+
+function changeClass(e) {
+    boxes.forEach(box => box.classList.remove('active'))
+    e.target.parentNode.classList.add('active')
+}
+
+boxes.forEach(box => {
+    box.addEventListener('click', changeClass)
+})
+
