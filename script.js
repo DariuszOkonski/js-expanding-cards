904e2b9608bf907a27fb05d9468bc932961c5336
Dariusz Okonski <darek200180@gmail.com>
2021-02-13 16:57:28 +0000

    finish script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const boxes = document.querySelectorAll('.box')
+const boxes = document.querySelectorAll('.box');
+const firstBox = document.getElementById('first-box');
+
+const cssVindowSize = getComputedStyle(document.documentElement).getPropertyValue('--minWindowSize')
 
 
 function changeClass(e) {
@@ -10,3 +13,11 @@ boxes.forEach(box => {
     box.addEventListener('click', changeClass)
 })
 
+// add class active to first element if window size < 720
+function reportResize() {
+    if (window.innerWidth <= cssVindowSize) {
+        boxes.forEach(box => box.classList.remove('active'))
+        firstBox.classList.add('active')
+    }
+}
+window.onresize = reportResize
